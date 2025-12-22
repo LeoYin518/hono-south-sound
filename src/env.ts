de0b953e35af38772e279d01d5f8661902351fcf
leoyin <yinzhengnan2021@163.com>
2025-12-22 11:16:10 +0800

    feat:扩展环境变量

---
+++ src/env.ts
@@ -0,0 +1,26 @@
+import { z } from 'zod';
+import { config } from 'dotenv'
+import { expand } from 'dotenv-expand'
+
+// 读取并扩展环境变量
+expand(config())
+
+const EnvSchema = z.object({
+    NODE_ENV: z.string().default('development'),
+    PORT: z.coerce.number().default(3000),
+});
+
+// 根据 EnvSchema 里面的属性接口自动生成一个 TS 类型
+export type Env = z.infer<typeof EnvSchema>;
+
+let env: Env;
+try {
+    env = EnvSchema.parse(process.env);
+} catch (e) {
+    const error = e as z.ZodError;
+    console.error('❌ Invalid environment variables:');
+    console.error(error.flatten().fieldErrors);
+    process.exit(1);
+}
+
+export default env;
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
 import { serve } from '@hono/node-server'
 import app from './app.js'
+import env from './env.js'
 
 serve({
   fetch: app.fetch,
-  port: 3000
+  port: env.PORT || 3000
 }, (info) => {
   console.log(`Server is running on http://localhost:${info.port}`)
 })
